f6b274c9ed3c122f6f9426af9cdf55ae9dd5b06e
grupweb1000 <grupweb1000@gmail.com>
2021-05-25 20:18:56 -0500

    Cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBH_FwwLnMQn-Zj7ucGOkq8IJCu0eSlrkA",
+  authDomain: "fbwebchat50.firebaseapp.com",
+  projectId: "fbwebchat50",
+  storageBucket: "fbwebchat50.appspot.com",
+  messagingSenderId: "67087943330",
+  appId: "1:67087943330:web:8b7e2c25bee64cd543f9fb",
+  measurementId: "G-FZ179HYQSE"
+    });
